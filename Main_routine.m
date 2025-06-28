c0175df15c60600420ceea179b01ff14930ebe40
ruchi-holker <ruchi.holker@gmail.com>
2025-06-28 11:28:41 +0530

    Update Main_routine.m

--- Main_routine.m
+++ Main_routine.m
@@ -36,6 +36,20 @@ ADHD_files = dir(fullfile(path, 'ADHD'));
 ADHD_files = ADHD_files(3:end-1);
 ADHD_files = ADHD_files(1:300);
 
+
+%% Load ADD
+d = dir([PATH, '\ADHD\']);
+d = d(3:end);
+
+for i= 1:length(d)
+    fcount = fprintf(['Training ADHD File:', num2str(i), '/', num2str(length(d))]);
+    eeg_file = fullfile(PATH, 'ADHD', d(i).name);
+    dlmwrite('ADHD_method-1.csv', [method_1(eeg_file).', 3], '-append');
+%     D_feat_vec2(:, i) = [method_2(eeg_file); 3];
+    dlmwrite('ADHD_method-3.csv', [method_3(eeg_file).', 3], '-append');
+    fprintf(repmat('\b', 1, fcount))
+end
+
 %% Feature extraction
 [p, q] = rat(fs/256);  % Resampling factor calculation
 EEG = cell(9, length(CTRL_files)+length(ADD_files)+length(ADHD_files));
@@ -56,36 +70,6 @@ for f = 1:length(CTRL_files)
 end
 save ADD_beforeCSP_all.mat features labels
 
-%% ADD file processing
-% features = zeros(K, 385, length(ADD_files));
-for f = 1:length(ADD_files)
-    fcount = fprintf(['Reading Train->ADD file: ', num2str(f), '/', num2str(length(ADD_files))]);
-    data = csvread(fullfile(path, 'ADD', ADD_files(f).name));
-%     data = resample(data, 25, 64);   % Resampling to 100 Hz (from paper)
-    X = abs(fft(data, NFFT, 1));
-    X = X(1:K, :).^2;
-    
-    features(:, :, f+length(CTRL_files)) = X;
-%     features(:, :, f) = X;
-    labels(f+length(CTRL_files)) = 1;
-    fprintf(repmat('\b', 1, fcount))
-end
-save CTRL_beforeCSP_all.mat features labels
-
-%% ADHD file processing
-% features = zeros(K, 385, length(ADHD_files));
-for f = 1:length(ADHD_files)
-    fcount = fprintf(['Reading Train->ADHD file: ', num2str(f), '/', num2str(length(ADHD_files))]);
-    data = csvread(fullfile(path, 'ADHD', ADHD_files(f).name));
-%     data = resample(data, 25, 64);   % Resampling to 100 Hz (from paper)
-    X = abs(fft(data, NFFT, 1));
-    X = X(1:K, :).^2;
-    
-    features(:, :, f+length(CTRL_files)+length(ADD_files)) = X;
-%     features(:, :, f) = X;
-    labels(f+length(CTRL_files)+length(ADD_files)) = 1;
-    fprintf(repmat('\b', 1, fcount))
-end
 
 fprintf('Feature extraction is complete.\n')
 save ADHD_beforeCSP_all.mat features labels
@@ -106,18 +90,5 @@ for bank = 1: size(fbank, 1)
     start = stop + 1;
 end
 fprintf('CSP processing is complete.\n')
-
-%% Linear Discriminant Analysis
-fcount = fprintf('Started LDA analysis.\n');
-Prj = reshape(Prj_X, [size(Prj_X, 1)*size(Prj_X, 2), size(Prj_X, 3)]);
-% [Y, EV, E] = LDA(Prj.', labels);
-fprintf(repmat('\b', 1, fcount))
-fprintf('LDA analysis complete.\n');
-
-%%
-csvwrite('filter_bank_ADHD_large.csv', [Prj; labels].');
-fprintf('Data saved.\n')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 %% END OF Main_routing.m
